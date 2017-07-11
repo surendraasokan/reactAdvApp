e77997ea3829593d29b43179931ef3ddcbc6d890
surendraasokan <sasokan@icct.com>
2017-07-11 12:16:26 -0400

    first commit

---
+++ src/components/posts_index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class Postsindex extends Component {
+  render() {
+    return (
+      <div>
+        Posts index
+      </div>
+    );
+
+  }
+}
--- src/index.js
+++ src/index.js
@@ -3,13 +3,22 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+import reducers from './reducers';
+import { BrowserRouter, Route } from 'react-router-dom';
+import PostsIndex from './components/posts_index';
+import promise from 'redux-promise';
+
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
+
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <Route path="/" component={PostsIndex} />
+      </div>
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
