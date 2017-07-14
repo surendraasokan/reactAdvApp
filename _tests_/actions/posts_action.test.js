6538567b2c3472ecf0dc9b17f157c1c6aefd691a
surendraasokan <sasokan@icct.com>
2017-07-14 10:22:00 -0400

    jest for actions

--- _tests_/actions/posts_action.test.js
+++ _tests_/actions/posts_action.test.js
@@ -2,41 +2,30 @@ import { fetchPosts } from '../../src/actions';
 import configureMockStore from 'redux-mock-store';
 import promise from 'redux-promise';
 import axios from 'axios';
-import * as AxiosMock from 'axios-mock-adapter';
+import MockAdapter from 'axios-mock-adapter';
 
 const mockStore = configureMockStore([promise]);
 const ROOT_URL = "http://reduxblog.herokuapp.com/api";
 const API_KEY = '?key=sarbein123';
 
 describe('AppActions Tests', () => {
-  beforeEach(() => {
-    const store = mockStore();
-    const mock = new AxiosMock(axios);
-  });
+
+  const store = mockStore();
+  let mock = new MockAdapter(axios);
 
   afterEach(() => {
     mock.restore();
   });
 
   it('handles fetchPosts success', () => {
-
-
-    // Mock any GET request to /users
-    // arguments for reply are (status, data, headers)
-    mock.onGet(`${ROOT_URL}/posts${API_KEY}`).reply(304,
+    mock.onGet(`${ROOT_URL}/posts${API_KEY}`).reply(200,
       [{"id":112320,"title":"Test post","categories":"test categories","content":"test content"}]);
 
     return store.dispatch(fetchPosts())
       .then(() => {
-        expect(store.getActions()).toEqual(
-          {
-            type: 'fetchPosts',
-            payload: {
-              data: [{"id":112320,"title":"Test post","categories":"test categories","content":"test content"}]
-            }
-          }
-        );
+        expect(store.getActions()[0].payload.data).toEqual(
+          [{"id":112320,"title":"Test post","categories":"test categories","content":"test content"}]);
       });
   });
 
-})
+});
