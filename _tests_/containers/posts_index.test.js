fd70770459e85303a574269276d935314831dcc6
surendraasokan <sasokan@icct.com>
2017-07-15 18:16:57 -0400

    jest changes

--- _tests_/containers/posts_index.test.js
+++ _tests_/containers/posts_index.test.js
@@ -1,9 +1,19 @@
 import { mount } from 'enzyme';
 import React, { Component } from 'react';
+import {connect} from 'react-redux';
+import { createStore } from 'redux';
+import { hashHistory } from 'react-router-dom';
 import PostsIndex from '../../src/containers/posts_index';
 
 describe('tests for the PostsIndex component',() => {
-  const wrapper = mount(<PostsIndex />);
+  const props = {
+    posts: [
+      {id:121, title:"Test one"},
+      {id:122, title:"Test two"}
+    ]
+  };
+  let store = createStore(() => ({}), ['Use Redux']);
+  const wrapper = mount(<PostsIndex history={hashHistory} {...props} store={store}/>);
 
   it('test for calling componentDidMount fn', () => {
     PostsIndex.prototype.componentDidMount = jest.fn();
--- _tests_/reducers/post_reducer.test.js
+++ _tests_/reducers/post_reducer.test.js
@@ -2,7 +2,43 @@ import { FETCH_POSTS, FETCH_POST, DELETE_POST } from '../../src/actions';
 import posts from '../../src/reducers/reducer_posts';
 describe("Posts Reducer Tests", () => {
 
-  it("FETCH_POSTS Success");
+  it("FETCH_POSTS Success", () => {
+    const state = {};
+    const action = {
+      type: FETCH_POSTS,
+      payload: {
+                data: [{
+                          id: 112320,
+                          title: "Test post",
+                          categories: "test categories",
+                          content: "test content"
+                        },
+                        {
+                          id: 112321,
+                          title: "Test1 post",
+                          categories: "test categories",
+                          content: "test content"
+                        }
+                      ]
+              }
+    }
+
+    const results = posts(state, action);
+    expect(results).toEqual({
+      "112320" : {
+        id: 112320,
+        title: "Test post",
+        categories: "test categories",
+        content: "test content"
+      },
+      "112321" : {
+        id: 112321,
+        title: "Test1 post",
+        categories: "test categories",
+        content: "test content"
+      }}); 
+
+  });
 
   it("DELETE_POST Success", () => {
     const state = {
@@ -21,7 +57,7 @@ describe("Posts Reducer Tests", () => {
     };
     const action = {
       type: DELETE_POST,
-      payload: "112321" 
+      payload: "112321"
     }
 
     const results = posts(state, action);
